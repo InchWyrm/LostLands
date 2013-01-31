699c7bd194e2cc977e235312125a206f8ab8d69b
tickerdee <ayountcse@yahoo.com>
2013-01-30 19:32:20 -0700

    done

--- LostLands/LostLands/LostLands/Game1.cs
+++ LostLands/LostLands/LostLands/Game1.cs
@@ -60,10 +60,6 @@ namespace LostLands
             // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// //Allows the game to run logic such as updating the world,
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
             // Allows the game to exit
