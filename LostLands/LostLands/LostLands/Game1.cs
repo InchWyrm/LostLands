840ed97571a0e9a3c4f6a36b3a3e6dff8094169d
tickerdee <ayountcse@yahoo.com>
2013-01-30 19:57:08 -0700

    a

--- LostLands/LostLands/LostLands/Game1.cs
+++ LostLands/LostLands/LostLands/Game1.cs
@@ -89,8 +89,6 @@ namespace LostLands
 
             newScreen.Draw(gameTime);
 
-            int counter = 0;
-
             spriteBatch.DrawString(newScreen.font1, newScreen.oldM.X + "," + newScreen.oldM.Y, new Vector2(20, 20), Color.Red);
                 
             spriteBatch.End();
