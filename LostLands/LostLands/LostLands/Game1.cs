0a575ad6e5755607a5e0dd5dc290089440a158b1
tickerdee <ayountcse@yahoo.com>
2013-01-30 19:06:16 -0700

    test

--- LostLands/LostLands/LostLands/Game1.cs
+++ LostLands/LostLands/LostLands/Game1.cs
@@ -62,7 +62,6 @@ namespace LostLands
 
         /// <summary>
         /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
         /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
